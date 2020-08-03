292b751b6c0059eea4c7a6dda4bfe7e6c6bb03ef
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2020-08-03 15:37:35 -0300

    first commit

---
+++ js/content.js
@@ -0,0 +1,106 @@
+const profile = {
+  "about": {
+    "name": "Vinicius",
+    "surname": "Guerrero",
+    "email": "vini.ts.guerrero@gmail.com",
+    "contact": "Software Engineer / Project Manager · +55 (11) 96894-4532 · ",
+    "description": "I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.",
+    "networks": {
+      "fab fa-linkedin-in": "https://www.linkedin.com/in/vinicius-guerrero-161409aa/",
+      "fab fa-github": "https://github.com/vini-guerrero",
+      "fab fa-twitter": "https://twitter.com/viiniiguerrero",
+      "fab fa-itch-io": "https://viniguerrero.itch.io/",
+      "fab fa-discord": "VinnyGuerrero#7237",
+      "fab fa-youtube": "https://www.youtube.com/channel/UCbEKIr_SrQriVr2zNnVTHTg"
+    }
+  },
+  "interests": [
+    "Acting in the implementation , maintenance and development of systems , applications and database , following good programming practices.",
+    "Self-learn system development based on .NET platform using C# programming language focusing on connected systems, object-oriented layer programming. Applications prototipes developed in WPF, MVC, Windows Forms, Views, Forms, Dashboard Alerts and Websites using HTMl5, CSS3, Javascript, JQuery, Bootstrap Framework, Bootbox.js, SignalR, NodeJS and AngularJS. Hybrid Mobile Apllication development using Ionic Framrwork, Cordova, Plugins with back-end based on JSON files structure using the Firebase platform. Windows Phone 8.0 projects using XAML. Database using Stored Procedures, Triggers and Scripts. Game development, 2D using sprites or 3D FBX models, scripts, servers on Windows platform or Handheld Consoles (PSP). Additional Softwares: ​Microsoft Visual Studio .NET, SQL Server, SQL Management Studio, MySql, Access, Nero Visio, Macromedia Flash MX, Gimp, Sony Vegas, Sublime Text, Dev-C++, Notepad++, Framework XNA, Framework Windows Phone, Unity 3D Engine, FPSC, Game Maker, Blender, Power Designer, Eclipse 3.3."
+  ],
+  "education": {
+    "Fatec Technology University of Guarulhos": {
+      "subheading": "Airport Logistics",
+      "description": "Technology - Complete",
+      "timeframe": "January 2009 - December 2011"
+    },
+    "Cruzeiro do Sul University": {
+      "subheading": "Software Development",
+      "description": "Technology - In Progress",
+      "timeframe": "January 2015 - Present"
+    }
+  },
+  "experiences": {
+    "First Performance Global": {
+      "subheading": "Resource IT",
+      "description": "Gogonogo project, containing mobile application, administrative portal and user service call center. Acting on both backend and frontend with a focus on Javascript technologies and frameworks, Node packages, ORM integrated with MySQL and PhpMyAdmin databases. White-label application architecture for several banking customers in countries such as the United States, Chile, Colombia, Brazil and Argentina, working with the reading and interpretation of bank transaction data and card control in APIs integrated with a front-end responsive. SCRUM development with Planning Poker and Requirements control via Target Process tool, daily project monitoring conferences. Customer support and log tracking.",
+      "timeframe": "April 2016 - Present"
+    },
+    "BRQ IT Services": {
+      "subheading": "Itaú Unibanco",
+      "description": "Project Splunk> Big Data Business Analytics project development, indexing real-time log data capture from relational or non-relational databases, XML, JSON, field extraction based on regular expressions, data analysis used to construct dashboards, views, forms, reports, alerts or graphics for events or security control measures interpretation. Project Carga Multicanal MVC System firmware upgrading solution for bank branches, providing task-schedule, filtering searches and user authentication. ActiveX component used to generate compressed packages. Windows Services and WCF agents acting in user control and SQL procedures and queries. Project Portal de Serviços MVC Web layout adaptation from a fluid-layout to a two fixed breakpoint resolution system, following browser resize changes during user navigation. Side menu change from a static dom element to a retractile JQuery UI based object.",
+      "timeframe": "July 2014 - January 2016"
+    },
+    "Benner Solution": {
+      "subheading": "Patrus Transportes Urgentes",
+      "description": "Project BL Logistics TMS / ERP System migration from Dephi architecture to .NET C# SQL table maintenance and file integration between systems. Firmware release validation, XML files reception using EDI technology, user support, data register in Supply Chain. Transport document emission (RPS /CTe). Travel, cargo and productivity report design.",
+      "timeframe": "March 2014 – July 2014"
+    },
+    "Tap Portugal": {
+      "subheading": "Orbital Serv. Auxiliares de Transporte Aéreo",
+      "description": "Internacional Airport Arrival Data manipulation using Worldtracer system, integrated platform to Amadeus. Customer register in PIR, AHL, DPR process, luggage customs clearance, air transport product routing and luggage tracking in national or international territory.",
+      "timeframe": "June 2011 – July 2012"
+    }
+  },
+  "certifications" : [
+    "Fluent English - Wizard Languages",
+    "Big Data - Intel Corporation",
+    "NETC# - Fatec Praia Grande",
+    "Spanish - Fatec Guarulhos",
+    "IT Management - Next Generation Center",
+    "IT Security - Next Generation Center",
+    "Software Development Process - Fundação Bradesco",
+    "Mobile Applications - Fundação Bradesco",
+    "C# Programming - Fundação Bradesco",
+    "Conectivity Concepts - Dígitro Tecnologia",
+    "IT In Organizations - FGV",
+    "TMS - Transport Management Systems - Benner Solution"
+  ],
+  "skills": {
+    "icons": [
+      "fab fa-linux",
+      "fab fa-windows",
+      "fab fa-node",
+      "fab fa-npm",
+      "fab fa-gulp",
+      "fab fa-php",
+      "fab fa-python",
+      "fab fa-html5",
+      "fab fa-css3-alt",
+      "fab fa-js-square",
+      "fab fa-bootstrap",
+      "fab fa-angular",
+      "fab fa-react",
+      "fab fa-vuejs",
+      "fab fa-android",
+      "fab fa-google-play",
+      "fab fa-apple",
+      "fab fa-app-store",
+      "fab fa-wordpress",
+      "fab fa-cpanel",
+      "fas fa-database",
+      "fab fa-git",
+      "fab fa-github",
+      "fab fa-bitbucket",
+      "fab fa-jira",
+      "fab fa-trello",
+      "fab fa-unity"
+    ],
+    "workflow": [
+      "Mobile-First, Responsive Design",
+      "Cross Browser Testing & Debugging",
+      "Cross Functional Teams",
+      "Agile Development & Scrum"
+    ]
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+  fillProfileInfo();
+  fillEducation();
+  fillExperience();
+  fillCertificate();
+});
+
+function fillProfileInfo(){
+  // Name
+  let name = "";
+  name += "<h1 class='mb-0'>" + profile.about.name;
+      name += "<span class='text-primary'> " + profile.about.surname + "</span>";
+  name += "</h1>";
+  $("#basic_info_name").html(name);
+  // Contact
+  let basic_info = "";
+  basic_info += "<div class='subheading mb-5'>";
+      basic_info += profile.about.contact;
+      basic_info += "<a href='mailto:" + profile.about.email + "'>" + profile.about.email + "</a>";
+  basic_info += "</div>";
+  basic_info += "<p>" + profile.about.description + "</p>";
+  $("#basic_info_description").html(basic_info);
+  // Interests
+  let interests_info = "";
+  for (var i = 0; i < profile.interests.length; i++) {
+    let interest = profile.interests[i];
+    interests_info += "<p>" + interest + "</p>";
+    interests_info += "<p class='mb-0'></p>";
+  }
+  $("#interests_info").html(interests_info);
+  // Networks
+  let networks_info = "";
+  let networks = profile.about.networks;
+  for (var icon in networks) {
+    let url = networks[icon];
+    networks_info += "<a class='social-icon' href='" + url + "'><i class='" + icon + "'></i></a>";
+  }
+  $(".social-icons").html(networks_info);
+  // Skills
+  let skills_info = "";
+  let skills = profile.skills.icons;
+  for (var icon in skills) {
+    skills_info += "<li class='list-inline-item'><i class='" + skills[icon] + "'></i></li>";
+  }
+  $(".dev-icons").html(skills_info);
+  // Wofkflows
+  let workflow_info = "";
+  let workflow = profile.skills.workflow;
+  for (var w in workflow) {
+    workflow_info += "<li>";
+        workflow_info += "<span class='fa-li'><i class='fas fa-check'></i></span>";
+        workflow_info += workflow[w];
+    workflow_info += "</li>";
+  }
+  $("#workflow_info").html(workflow_info);
+}
+
+function fillEducation(){
+  let education_field = $("#education_info");
+  let content = "";
+  let education = profile.education;
+  for (var ed in education) {
+    let title = ed;
+    let subheading = education[ed]["subheading"];
+    let description = education[ed]["description"];
+    let timeframe = education[ed]["timeframe"];
+    content += "<div class='d-flex flex-column flex-md-row justify-content-between mb-5'>";
+        content += "<div class='flex-grow-1'>";
+            content += "<h3 class='mb-0'>" + title + "</h3>";
+            content += "<div class='subheading mb-3'>" + subheading + "</div>";
+            content += "<div>" + description + "</div>";
+        content += "</div>";
+        content += "<div class='flex-shrink-0'><span class='text-primary'>" + timeframe + "</span></div>";
+    content += "</div>";
+  }
+  $(education_field).html(content);
+}
+
+function fillExperience(){
+  let exp_field = $("#experience_info");
+  let content = "";
+  let experiences = profile.experiences;
+  for (var exp in experiences) {
+    let title = exp;
+    let subheading = experiences[exp]["subheading"];
+    let description = experiences[exp]["description"];
+    let timeframe = experiences[exp]["timeframe"];
+    content += "<div class='d-flex flex-column flex-md-row justify-content-between mb-5'>";
+    content += "<div class='flex-grow-1'>";
+    content += "<h3 class='mb-0'>" + title + "</h3>";
+    content += "<div class='subheading mb-3'>" + subheading + "</div>";
+    content += "<p>" + description + "</p>";
+    content += "</div>";
+    content += "<div class='flex-shrink-0'><span class='text-primary'>" + timeframe + "</span></div>";
+    content += "</div>";
+  }
+  $(exp_field).html(content);
+}
+
+function fillCertificate(){
+  let cert_field = $("#certifications");
+  let content = "";
+  let certifications = profile.certifications;
+  for (var i = 0; i < certifications.length; i++) {
+    content += "<li>";
+        content += "<span class='fa-li'><i class='fas fa-trophy text-warning'></i></span>";
+        content += certifications[i];
+    content += "</li>";
+  }
+  $(cert_field).html(content);
+}
