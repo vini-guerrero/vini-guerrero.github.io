4a6198cc5a36e5f090166106acab2592abb49aed
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-03-29 16:53:13 -0300

    Added Portfolio Description

--- content/data.js
+++ content/data.js
@@ -1,62 +1,72 @@
 const profile = {
-  "about": {
-    "name": "Vinicius",
-    "surname": "Guerrero",
-    "email": "vini.ts.guerrero@gmail.com",
-    "contact": "Software Engineer / Project Manager Office  ·  ",
-    "description": "Acting in the implementation, maintenance and development of systems, applications and database, following good programming practices. Experience in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.",
-    "networks": {
-      "fab fa-linkedin-in": "https://www.linkedin.com/in/vinicius-guerrero-161409aa/",
+  about: {
+    name: "Vinicius",
+    surname: "Guerrero",
+    email: "vini.ts.guerrero@gmail.com",
+    contact: "Software Engineer / Project Manager Office  ·  ",
+    description:
+      "Acting in the implementation, maintenance and development of systems, applications and database, following good programming practices. Experience in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.",
+    networks: {
+      "fab fa-linkedin-in":
+        "https://www.linkedin.com/in/vinicius-guerrero-161409aa/",
       "fab fa-github": "https://github.com/vini-guerrero",
       "fab fa-twitter": "https://twitter.com/viniguerrero",
       "fab fa-itch-io": "https://viniguerrero.itch.io/",
       "fab fa-discord": "VinnyGuerrero#7237",
-      "fab fa-youtube": "https://www.youtube.com/channel/UCbEKIr_SrQriVr2zNnVTHTg"
-    }
+      "fab fa-youtube":
+        "https://www.youtube.com/channel/UCbEKIr_SrQriVr2zNnVTHTg",
+    },
   },
-  "interests": [
-    "Self-learn system development based on .NET platform using C# programming language focusing on connected systems, object-oriented layer programming. Applications prototipes developed in WPF, MVC, Windows Forms, Views, Forms, Dashboard Alerts and Websites using HTMl5, CSS3, Javascript, JQuery, Bootstrap Framework, Bootbox.js, SignalR, NodeJS and AngularJS. Hybrid Mobile Apllication development using Ionic Framework, Cordova, Ionic, Plugins with back-end based on JSON files structure using the Firebase platform. Windows Phone 8.0 projects using XAML. Database using Stored Procedures, Triggers and Scripts. Game development, 2D using sprites or 3D FBX models, scripts, servers on Windows platform or Handheld Consoles (PSP). Additional Softwares: ​Microsoft Visual Studio .NET, SQL Server, SQL Management Studio, MySql, Access, Nero Visio, Macromedia Flash MX, Gimp, Sony Vegas, Sublime Text, Dev-C++, Notepad++, Framework XNA, Framework Windows Phone, Godot Engine, Unreal Engine, Unity, FPSC, Game Maker, Blender, Power Designer, Eclipse."
+  interests: [
+    "Self-learn system development based on .NET platform using C# programming language focusing on connected systems, object-oriented layer programming. Applications prototipes developed in WPF, MVC, Windows Forms, Views, Forms, Dashboard Alerts and Websites using HTMl5, CSS3, Javascript, JQuery, Bootstrap Framework, Bootbox.js, SignalR, NodeJS and AngularJS. Hybrid Mobile Apllication development using Ionic Framework, Cordova, Ionic, Plugins with back-end based on JSON files structure using the Firebase platform. Windows Phone 8.0 projects using XAML. Database using Stored Procedures, Triggers and Scripts. Game development, 2D using sprites or 3D FBX models, scripts, servers on Windows platform or Handheld Consoles (PSP). Additional Softwares: ​Microsoft Visual Studio .NET, SQL Server, SQL Management Studio, MySql, Access, Nero Visio, Macromedia Flash MX, Gimp, Sony Vegas, Sublime Text, Dev-C++, Notepad++, Framework XNA, Framework Windows Phone, Godot Engine, Unreal Engine, Unity, FPSC, Game Maker, Blender, Power Designer, Eclipse.",
   ],
-  "education": {
+  education: {
     "Fatec Technology University of Guarulhos": {
-      "subheading": "Airport Logistics",
-      "description": "Responsible for planning and controlling cargo handling and storage at an airport. Evaluates the best air transport and packaging option for each type of merchandise. Defines how loads will be loaded. It determines the organization within specific containers for air transport. Calculates freight costs and tariffs. It also analyzes airline balance sheets. Another front of activity is the passenger terminal, where the technologist works with the planning of the flow of people and luggage. In Brazil, the privatization of airports stimulated the growth and professionalization of the sector, opening the field for the technologist in Airport Logistics.",
-      "timeframe": "Technology - Complete"
+      subheading: "Airport Logistics",
+      description:
+        "Responsible for planning and controlling cargo handling and storage at an airport. Evaluates the best air transport and packaging option for each type of merchandise. Defines how loads will be loaded. It determines the organization within specific containers for air transport. Calculates freight costs and tariffs. It also analyzes airline balance sheets. Another front of activity is the passenger terminal, where the technologist works with the planning of the flow of people and luggage. In Brazil, the privatization of airports stimulated the growth and professionalization of the sector, opening the field for the technologist in Airport Logistics.",
+      timeframe: "Technology - Complete",
     },
     "Cruzeiro do Sul University": {
-      "subheading": "Software Development",
-      "description": "Responsible for developing, designing, analyzing, implementing and maintaining information systems in different sectors. Information systems are those responsible for managing, organizing and manipulating information and data relevant to a specific sector or even an entire company. They work both in environments such as the internet and also on corporate intranet. These systems may be responsible for managing, for example, a company's inventory control data or even all information pertaining to the human resources sector database. The systems analyst is responsible for developing this software, from computer programming to integration with other machines, modeling these systems. However, the professional also needs to be based on notions of entrepreneurship, seeking to understand customers when developing systems that meet their needs. It is also important to have a certain amount of creativity to overcome possible obstacles. As the Systems Analysis and Development technologist has a strong programming base, developing games for mobile devices also presents itself as a job possibility for him.",
-      "timeframe": "Technology - In Progress"
-    }
+      subheading: "Software Development",
+      description:
+        "Responsible for developing, designing, analyzing, implementing and maintaining information systems in different sectors. Information systems are those responsible for managing, organizing and manipulating information and data relevant to a specific sector or even an entire company. They work both in environments such as the internet and also on corporate intranet. These systems may be responsible for managing, for example, a company's inventory control data or even all information pertaining to the human resources sector database. The systems analyst is responsible for developing this software, from computer programming to integration with other machines, modeling these systems. However, the professional also needs to be based on notions of entrepreneurship, seeking to understand customers when developing systems that meet their needs. It is also important to have a certain amount of creativity to overcome possible obstacles. As the Systems Analysis and Development technologist has a strong programming base, developing games for mobile devices also presents itself as a job possibility for him.",
+      timeframe: "Technology - In Progress",
+    },
   },
-  "experiences": {
+  experiences: {
     "First Performance Global": {
-      "subheading": "AlphaTech Studio",
-      "description": "Solutions for digital engagement in the branch of bank transaction authorizers. Advanced financial controls, experiences for cardholders with a management panel for customers from different countries and languages, call center, mobile applications and creation of machine learning procedures.",
-      "timeframe": "November 2019 - Present"
+      subheading: "AlphaTech Studio",
+      description:
+        "Solutions for digital engagement in the branch of bank transaction authorizers. Advanced financial controls, experiences for cardholders with a management panel for customers from different countries and languages, call center, mobile applications and creation of machine learning procedures.",
+      timeframe: "November 2019 - Present",
     },
     "Resource IT Solutions": {
-      "subheading": "First Performance Global",
-      "description": "Mobile application, administrative portal and user service call center for Gogonogo project. Acting on both backend and frontend with a focus on Javascript technologies and frameworks, Node packages, ORM integrated with MySQL and PhpMyAdmin databases. White-label application architecture for several banking customers in countries such as the United States, Chile, Colombia, Brazil and Argentina, working with the reading and interpretation of bank transaction data and card control in APIs integrated with a front-end responsive. SCRUM development with Planning Poker and Requirements control via Target Process tool, daily project monitoring conferences. Customer support and log tracking.",
-      "timeframe": "April 2016 - November 2019"
+      subheading: "First Performance Global",
+      description:
+        "Mobile application, administrative portal and user service call center for Gogonogo project. Acting on both backend and frontend with a focus on Javascript technologies and frameworks, Node packages, ORM integrated with MySQL and PhpMyAdmin databases. White-label application architecture for several banking customers in countries such as the United States, Chile, Colombia, Brazil and Argentina, working with the reading and interpretation of bank transaction data and card control in APIs integrated with a front-end responsive. SCRUM development with Planning Poker and Requirements control via Target Process tool, daily project monitoring conferences. Customer support and log tracking.",
+      timeframe: "April 2016 - November 2019",
     },
     "BRQ IT Services": {
-      "subheading": "Itaú Unibanco",
-      "description": "Splunk> Big Data Business Analytics project development, indexing real-time log data capture from relational or non-relational databases, XML, JSON, field extraction based on regular expressions, data analysis used to construct dashboards, views, forms, reports, alerts or graphics for events or security control measures interpretation. Project Carga Multicanal MVC System firmware upgrading solution for bank branches, providing task-schedule, filtering searches and user authentication. ActiveX component used to generate compressed packages. Windows Services and WCF agents acting in user control and SQL procedures and queries. Project Portal de Serviços MVC Web layout adaptation from a fluid-layout to a two fixed breakpoint resolution system, following browser resize changes during user navigation. Side menu change from a static dom element to a retractile JQuery UI based object.",
-      "timeframe": "July 2014 - January 2016"
+      subheading: "Itaú Unibanco",
+      description:
+        "Splunk> Big Data Business Analytics project development, indexing real-time log data capture from relational or non-relational databases, XML, JSON, field extraction based on regular expressions, data analysis used to construct dashboards, views, forms, reports, alerts or graphics for events or security control measures interpretation. Project Carga Multicanal MVC System firmware upgrading solution for bank branches, providing task-schedule, filtering searches and user authentication. ActiveX component used to generate compressed packages. Windows Services and WCF agents acting in user control and SQL procedures and queries. Project Portal de Serviços MVC Web layout adaptation from a fluid-layout to a two fixed breakpoint resolution system, following browser resize changes during user navigation. Side menu change from a static dom element to a retractile JQuery UI based object.",
+      timeframe: "July 2014 - January 2016",
     },
     "Benner Solution": {
-      "subheading": "Patrus Transportes Urgentes",
-      "description": "BL Logistics TMS / ERP System migration from Dephi architecture to .NET C# SQL table maintenance and file integration between systems. Firmware release validation, XML files reception using EDI technology, user support, data register in Supply Chain. Transport document emission (RPS /CTe). Travel, cargo and productivity report design.",
-      "timeframe": "March 2014 – July 2014"
+      subheading: "Patrus Transportes Urgentes",
+      description:
+        "BL Logistics TMS / ERP System migration from Dephi architecture to .NET C# SQL table maintenance and file integration between systems. Firmware release validation, XML files reception using EDI technology, user support, data register in Supply Chain. Transport document emission (RPS /CTe). Travel, cargo and productivity report design.",
+      timeframe: "March 2014 – July 2014",
     },
     "Tap Portugal": {
-      "subheading": "Orbital Serv. Auxiliares de Transporte Aéreo",
-      "description": "Internacional Airport Arrival Data manipulation using Worldtracer system, integrated platform to Amadeus. Customer register in PIR, AHL, DPR process, luggage customs clearance, air transport product routing and luggage tracking in national or international territory.",
-      "timeframe": "June 2011 – July 2012"
-    }
+      subheading: "Orbital Serv. Auxiliares de Transporte Aéreo",
+      description:
+        "Internacional Airport Arrival Data manipulation using Worldtracer system, integrated platform to Amadeus. Customer register in PIR, AHL, DPR process, luggage customs clearance, air transport product routing and luggage tracking in national or international territory.",
+      timeframe: "June 2011 – July 2012",
+    },
   },
-  "certifications" : [
+  certifications: [
     "Fluent English - Wizard Languages",
     "Big Data - Intel Corporation",
     "Spanish - Fatec Guarulhos",
@@ -86,10 +96,10 @@ const profile = {
     "Microsoft Word - Curso Em Video",
     "PHP - Curso Em Video",
     "HTML5 - Curso Em Video",
-    "Home Office - Resource IT Solutions"
+    "Home Office - Resource IT Solutions",
   ],
-  "skills": {
-    "icons": [
+  skills: {
+    icons: [
       "fab fa-apple",
       "fab fa-linux",
       "fab fa-windows",
@@ -127,41 +137,61 @@ const profile = {
       "fab fa-edge",
       "fab fa-docker",
       "fab fa-digital-ocean",
-      "fab fa-google-drive",            
+      "fab fa-google-drive",
       "fab fa-unity",
-      "fab fa-itch-io"
+      "fab fa-itch-io",
     ],
-    "workflow": [
+    workflow: [
       "Mobile-First, Responsive Design",
       "Cross Browser Testing & Debugging",
       "Cross Functional Teams",
-      "Agile Development & Scrum"
-    ]
+      "Agile Development & Scrum",
+    ],
   },
-  "portfolio": {
-    "FP1": {
-      "url": "https://firstperformance.com/",
-      "img": "https://firstperformance.com/wp-content/uploads/2019/08/mobile-card-close-1.png"
+  portfolio: {
+    FP1: {
+      url: "https://firstperformance.com/",
+      img:
+        "https://firstperformance.com/wp-content/uploads/2019/08/mobile-card-close-1.png",
     },
-    "FP2": {
-      "url": "https://firstperformance.com/",
-      "img": "https://firstperformance.com/wp-content/uploads/2019/09/Next-Generation-of-Card-Controls-1.png"
+    FP2: {
+      url: "https://firstperformance.com/",
+      img:
+        "https://firstperformance.com/wp-content/uploads/2019/09/Next-Generation-of-Card-Controls-1.png",
     },
-    "Avalae1": {
-      "url": "https://avalae.com/",
-      "img": "assets/portfolio/1.png"
+    Avalae1: {
+      url: "https://avalae.com/",
+      img: "assets/portfolio/1.png",
     },
-    "Avalae2": {
-      "url": "https://avalae.com/",
-      "img": "assets/portfolio/2.png"
+    Avalae2: {
+      url: "https://avalae.com/",
+      img: "assets/portfolio/2.png",
     },
-    "GGT": {
-      "url": "https://viniguerrero.itch.io/godot-game-tools",
-      "img": "assets/portfolio/3.png"
+    GGT: {
+      url: "https://viniguerrero.itch.io/godot-game-tools",
+      img: "assets/portfolio/3.png",
     },
-    "PRC": {
-      "url": "https://viniguerrero.itch.io/pre-render-creator",
-      "img": "assets/portfolio/4.png"
-    }
-  }
-}
+    PRC: {
+      url: "https://viniguerrero.itch.io/pre-render-creator",
+      img: "assets/portfolio/4.png",
+    },
+  },
+  portfolio_items: {
+    "Godot Game Tools - GGT": [
+      "With over 25k views and almost 9k downloads only on Itch.io this tool simplifies the preparation of animated characters 3d models by creating a fast workflow as a Blender add-on that imports and exports gamedev-ready assets to include in multi-purpose projects.",
+      "⦿ Originally designed to Godot Engine, this tool has also been used on a numerous projects over other engines such as Unity or Unreal.",
+      "⦿ Batch and bake animations into a single file aiming for a significant reduction over final package as well as better file organization.",
+      "⦿ Fast and easy animation manager that includes quick preview, renaming, animation trimming, keyframe reduction, animation listing and more.",
+      "⦿ Support for Godot Engine Animation State Machine by generating configuration files that can be reused for as many characters as required by the project",
+      "⦿ Open source available and open to contributions project on <a href='https://github.com/vini-guerrero/Godot_Game_Tools' target='_blank'>Github</a>",
+      "Honorable mentions as reviews and tutorials on Canadians, Americans, Brazilian and Italian Youtube Channels:",
+      "<a href='https://youtu.be/utXuYpyBRN4' target='_blank'>✓ GamesfromScratch </a>",
+      "<a href='https://youtu.be/EmQBLxxPV3E' target='_blank'>✓ Ombarus </a>",
+      "<a href='https://youtu.be/wddbzgPouM8' target='_blank'>✓ Chili Turtle </a>",
+      "<a href='https://youtu.be/REYhm82x1VA' target='_blank'>✓ Blender Lowpoly </a>",
+      "<a href='https://youtu.be/UEWljYXbY3A' target='_blank'>✓ DevIsFunny </a>",
+    ],
+    // "Pre Render Creator - PRC": [],
+    // "Asset Builder": [],
+  },
+};
